036ed10cdcd90aeaf46597b222c0d53628bb642c
cre-inoue <inoue@hokkaido-ima.co.jp>
2018-05-17 22:51:15 +0900

    コミット

--- source/ccbc-web/src/view/header.js
+++ source/ccbc-web/src/view/header.js
@@ -1,17 +1,86 @@
 import React, { Component } from 'react'
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui-icons/Menu'
+import Drawer from 'material-ui/Drawer'
+import List from 'material-ui/List'
+import Divider from 'material-ui/Divider'
+import {
+  mailFolderListItems,
+  otherMailFolderListItems,
+  kanriListItems,
+  ippanListItems
+} from './tileData'
 
-export default class HeaderForm extends Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-        <AppBar title="MVPシステム">
-          <a href="/menu">メニューに戻る</a>&nbsp;&nbsp;
-          <a href="/">ログオフ</a>
-        </AppBar>
-      </MuiThemeProvider>
-    )
-  }
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    //height: 500,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex'
+  },
+  flex: {
+    flex: 1
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    minWidth: 0 // So the Typography noWrap works
+  },
+  toolbar: theme.mixins.toolbar
+})
+
+function ButtonAppBar(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            Most Valuable Player Vote System
+          </Typography>
+          <Button variant="raised" color="inherit" color="secondary" href="../">
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.toolbar} />
+      </main>
+    </div>
+  )
 }
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(ButtonAppBar)
--- source/ccbc-web/src/view/login.js
+++ source/ccbc-web/src/view/login.js
@@ -1,8 +1,130 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import MenuItem from 'material-ui/Menu/MenuItem'
+import TextField from 'material-ui/TextField'
+import Button from 'material-ui/Button'
+import FileUpload from 'material-ui-icons/FileUpload'
 
-export default class LoginForm extends Component {
+import Table from 'material-ui/Table'
+import TableBody from 'material-ui/Table/TableBody'
+import TableCell from 'material-ui/Table/TableCell'
+import TableHead from 'material-ui/Table/TableHead'
+import TableRow from 'material-ui/Table/TableRow'
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  },
+  iconSmall: {
+    fontSize: 20
+  },
+  table: {
+    minWidth: 700
+  }
+})
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$'
+  },
+  {
+    value: 'EUR',
+    label: '€'
+  },
+  {
+    value: 'BTC',
+    label: '฿'
+  },
+  {
+    value: 'JPY',
+    label: '¥'
+  }
+]
+
+class TextFields extends React.Component {
+  state = {
+    name: 'Cat in the Hat',
+    age: '',
+    multiline: 'Controlled',
+    currency: 'EUR'
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
   render() {
-    return <a href="/menu">ログイン</a>
+    const { classes } = this.props
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <div>
+          <Table className={classes.table}>
+            <TableBody>
+              <TableRow>
+                <TextField
+                  id="name"
+                  label="Name"
+                  className={classes.textField}
+                  onChange={this.handleChange('name')}
+                  fullWidth
+                  margin="normal"
+                />
+              </TableRow>
+              <TableRow>
+                <TextField
+                  id="password-input"
+                  label="Password"
+                  className={classes.textField}
+                  type="password"
+                  autoComplete="current-password"
+                  fullWidth
+                  margin="normal"
+                />
+              </TableRow>
+              <TableRow>
+                <Button
+                  className={classes.button}
+                  variant="raised"
+                  color="default"
+                  href="/appbar"
+                >
+                  login
+                  <FileUpload className={classes.rightIcon} />
+                </Button>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </form>
+    )
   }
 }
+
+TextFields.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(TextFields)
