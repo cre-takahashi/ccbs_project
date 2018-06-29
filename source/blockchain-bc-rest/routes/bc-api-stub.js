eb79bcecc2e22976eee7d59a86c61f5df162ed40
cre-inoue <38461462+cre-inoue@users.noreply.github.com>
2018-06-29 17:26:40 +0900

    スタブ最新化

--- source/blockchain-bc-rest/routes/bc-api-stub.js
+++ source/blockchain-bc-rest/routes/bc-api-stub.js
@@ -1,36 +1,115 @@
 /**
  * ブロックチェーンAPIスタブ
  */
-var request = require("superagent");
-var express = require("express");
-var router = express.Router();
+var request = require('superagent')
+var express = require('express')
+var router = express.Router()
 
 /**
+ * ------------------------------------------------------------
  * ユーザ登録（BCへのアカウント登録）
+ * ------------------------------------------------------------
+ * Param  : [ password ]   - パスワード
+ * Return : [ bc_account ] - アカウント
+ *          [ result ]     - 結果(true/false)
+ *          [ message ]    - エラーメッセージ(エラー時のみ)
+ * ------------------------------------------------------------
  */
-router.post("/add_account", function(req, res, err) {
-  console.log("★add_account:param:" + JSON.stringify(req.body));
-  res.json({ bc_account: "0xtest000000000000000000000000000000000000" });
-});
+router.post('/add_account', function (req, res, err) {
+  console.log("★add_account:param:" + JSON.stringify(req.body))
+
+  // 正常時
+  res.json({ bc_account: "0xtest000000000000000000000000000000000000", result: true })
+  // エラー時
+  //res.json({ result: false, message: "エラーです。" })
+})
 
 /**
+ * ------------------------------------------------------------
  * ログイン（BCへの認証）
+ * ------------------------------------------------------------
+ * Param  : [ account ]  - アカウント
+ *          [ password ] - パスワード
+ * Return : [ result ]   - 結果(true/false)
+ *          [ message ]  - エラーメッセージ(エラー時のみ)
+ * ------------------------------------------------------------
  */
-router.post("/login", function(req, res, err) {
-  console.log("★login:param:" + JSON.stringify(req.body));
-  res.json({ result: true });
-});
+router.post('/login', function (req, res, err) {
+  console.log("★login:param:" + JSON.stringify(req.body))
+
+  // 正常時(ログイン成功)
+  res.json({ result: true })
+  // 正常時(通常のログイン失敗)
+  //res.json({ result: false })
+  // エラー時
+  //res.json({ result: false, message: "エラーです。" })
+})
 
 /**
+ * ------------------------------------------------------------
  * コイン送金
+ * ------------------------------------------------------------
+ * Param  : [ from_account[] ] - 送金元アカウント(配列)
+ *          [ password[] ]     - 送金元パスワード(配列)
+ *          [ to_account[] ]   - 送金先アカウント(配列)
+ *          [ coin[] ]         - 送金コイン数(配列)
+ * Return : [ transaction[] ]  - トランザクションID(配列)
+ *          [ result ]         - 結果(true/false)
+ *          [ message ]        - エラーメッセージ(エラー時のみ)
+ * ------------------------------------------------------------
  */
-router.post("/send_coin", function(req, res, err) {
-  console.log("send_coin:param:" + JSON.stringify(req.body));
-  var fromAccount = req.body.from_account;
-  var toAccount = req.body.to_account;
-  var password = req.body.password;
-  var coin = req.body.coin;
-  res.json({ transaction: "test_transaction" });
-});
+router.post('/send_coin', function (req, res, err) {
+  console.log("send_coin:param:" + JSON.stringify(req.body))
+  var toAccounts = req.body.to_account
+  var resTransactions = new Array(toAccounts.length)
+  var i = 0
+  while (i < toAccounts.length) {
+    resTransactions[i] = "0xtest000000000000000000000000000000000000000000000000000000000000"
+    i++
+  }
+  // 正常時
+  res.json({ transaction: resTransactions, result: true })
+  // エラー時
+  //res.json({ transaction: new Array(toAccounts.length), result: false, message: "エラーです。" })
+})  
 
-module.exports = router;
+/**
+ * ------------------------------------------------------------
+ * コイン照会（現在のコイン）
+ * ------------------------------------------------------------
+ * Param  : [ account ] - アカウント
+ * Return : [ coin ]    - コイン数
+ *          [ result ]  - 結果(true/false)
+ *          [ message ] - エラーメッセージ(エラー時のみ)
+ * ------------------------------------------------------------
+ */
+router.post('/get_coin', function (req, res, err) {
+  console.log("get_coin:param:" + JSON.stringify(req.body))
+
+  // 正常時
+  res.json({ coin: 100, result: true })
+  // エラー時
+  //res.json({ coin: 0, result: false, message: "エラーです。" })
+})
+
+/**
+ * ------------------------------------------------------------
+ * コイン照会（年度単位のコイン）
+ * ------------------------------------------------------------
+ * Param  : [ account ] - アカウント
+ *          [ year ]    - 年度
+ * Return : [ coin ]    - コイン数
+ *          [ result ]  - 結果(true/false)
+ *          [ message ] - エラーメッセージ(エラー時のみ)
+ * ------------------------------------------------------------
+ */
+router.post('/get_coin_year', function (req, res, err) {
+  console.log("get_coin:param:" + JSON.stringify(req.body))
+
+  // 正常時
+  res.json({ coin: 2000, result: true })
+  // エラー時
+  //res.json({ coin: 0, result: false, message: "エラーです。" })
+})
+
+module.exports = router
