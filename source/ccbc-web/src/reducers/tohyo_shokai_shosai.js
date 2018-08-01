ebb3656fa06a0c586cdfee7b9fc934ae2ca9ba93
cre-inoue <38461462+cre-inoue@users.noreply.github.com>
2018-08-01 21:48:54 +0900

    イテ2マージ

---
+++ source/ccbc-web/src/reducers/tohyo_shokai_shosai.js
@@ -0,0 +1,10 @@
+const initialState = {
+  t_senkyo_pk: 1,
+  t_presenter_pk: 1,
+  t_rank: 1,
+  t_totalcoin: 500
+}
+
+export function tohyo_shokai_shosaiReducer(state = initialState, action) {
+  return state
+}
