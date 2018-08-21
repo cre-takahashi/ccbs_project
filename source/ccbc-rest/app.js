e73a30138f136afc192aeb80679ae148ab7accda
cre-inoue <38461462+cre-inoue@users.noreply.github.com>
2018-08-21 13:34:56 +0900

    クロスドメイン対応

--- source/ccbc-rest/app.js
+++ source/ccbc-rest/app.js
@@ -24,6 +24,28 @@ const tohyo_shokai_nendoRouter = require('./routes/tohyo_shokai_nendo')
 
 const app = express()
 
+/**
+ \* Cross-Origin Resource Sharingを有効にする記述（HTTPレスポンスヘッダの追加）
+ \*/
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Max-Age', '86400')
+  next()
+})
+
+/**
+ \* OPTIONSメソッドの実装
+ \*/
+app.options('*', function(req, res) {
+  res.sendStatus(200)
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
