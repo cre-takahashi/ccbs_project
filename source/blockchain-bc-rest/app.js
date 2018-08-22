642f2f702c3c0bc76fbb1b7643d49f3343189280
cre-inoue <38461462+cre-inoue@users.noreply.github.com>
2018-08-22 10:41:47 +0900

    Add files via upload

--- source/blockchain-bc-rest/app.js
+++ source/blockchain-bc-rest/app.js
@@ -1,60 +1,60 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var index = require("./routes/index");
-var users = require("./routes/users");
-var api = require("./routes/bc-api");
-var api_stub = require("./routes/bc-api-stub");
+var index = require('./routes/index');
+var users = require('./routes/users');
+var api = require('./routes/bc-api');
+var api_stub = require('./routes/bc-api-stub');
 
 var app = express();
 
 /**
  \* Cross-Origin Resource Sharingを有効にする記述（HTTPレスポンスヘッダの追加）
  \*/
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+ app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Max-Age", "86400");
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Max-Age', '86400')
+  next()
+})
 
 /**
  \* OPTIONSメソッドの実装
  \*/
-app.options("*", function(req, res) {
-  res.sendStatus(200);
-});
+app.options('*', function(req, res) {
+  res.sendStatus(200)
+})
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", index);
-app.use("/users", users);
+app.use('/', index);
+app.use('/users', users);
 
-app.use("/bc-api", api);
-//app.use('/bc-api', api_stub);
+//app.use('/bc-api', api);
+app.use('/bc-api', api_stub);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -63,11 +63,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
